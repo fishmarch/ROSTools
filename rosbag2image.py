4dfb13c569a80c6a3a514ee2a7fa452a01570903
fishmarch <zhang.xy@link.cuhk.edu.hk>
2025-02-14 16:06:04 +0800

    add bag lists

--- rosbag2image.py
+++ rosbag2image.py
@@ -7,9 +7,14 @@ from cv_bridge import CvBridge
 from cv_bridge import CvBridgeError
 from tqdm import tqdm
 
-path = "/home/hkclr/myData/VBR/campus_train1/"
-bag_path = '/home/hkclr/myData/VBR/campus_train1/campus_train1_00.bag'
-
+path = "/home/hkclr/myData/VBR/campus_train0/"
+bag_list = [
+    '/home/hkclr/myData/VBR/campus_train0/campus_train0_00.bag',
+    '/home/hkclr/myData/VBR/campus_train0/campus_train0_01.bag',
+    '/home/hkclr/myData/VBR/campus_train0/campus_train0_02.bag',
+    '/home/hkclr/myData/VBR/campus_train0/campus_train0_03.bag',
+    '/home/hkclr/myData/VBR/campus_train0/campus_train0_04.bag',
+    ]
 
 class ImageCreator():
     def __init__(self):
@@ -19,56 +24,57 @@ class ImageCreator():
         self.bridge = CvBridge()
         print("Start!")
         # dim = (640, 360)
-        with rosbag.Bag(bag_path, 'r') as bag:
-            total_messages = bag.get_message_count(topic_filters=["/camera_left/image_raw",
-                                                                  "/camera_right/image_raw",
-                                                                  "/imu/data"])
-            with tqdm(total=total_messages) as pbar:
-                for topic, msg, t in bag.read_messages():
-                    if topic == "/camera_left/image_raw":
-                        try:
-                            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-                            # cv_image = self.bridge.imgmsg_to_cv2(msg, "mono8")
-                        except CvBridgeError as e:
-                            print(e)
-                        # cv_image_resized = cv2.resize(cv_image, dim)
-                        timestr = str(msg.header.stamp.to_sec())
-                        image_info = timestr + " left/" + timestr + ".png\n"
-                        left_file.write(image_info)
-                        image_name = path + "left/" + timestr + ".png"
-                        cv2.imwrite(image_name, cv_image)
-                        pbar.update(1)
+        for bag_path in bag_list:
+            with rosbag.Bag(bag_path, 'r') as bag:
+                total_messages = bag.get_message_count(topic_filters=["/camera_left/image_raw",
+                                                                      "/camera_right/image_raw",
+                                                                      "/imu/data"])
+                with tqdm(total=total_messages) as pbar:
+                    for topic, msg, t in bag.read_messages():
+                        if topic == "/camera_left/image_raw":
+                            try:
+                                cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+                                # cv_image = self.bridge.imgmsg_to_cv2(msg, "mono8")
+                            except CvBridgeError as e:
+                                print(e)
+                            # cv_image_resized = cv2.resize(cv_image, dim)
+                            timestr = str(msg.header.stamp.to_sec())
+                            image_info = timestr + " left/" + timestr + ".png\n"
+                            left_file.write(image_info)
+                            image_name = path + "left/" + timestr + ".png"
+                            cv2.imwrite(image_name, cv_image)
+                            pbar.update(1)
 
-                    elif topic == "/camera_right/image_raw":
-                        try:
-                            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-                            # cv_image = self.bridge.imgmsg_to_cv2(msg, "mono8")
-                        except CvBridgeError as e:
-                            print(e)
-                        # cv_image_resized = cv2.resize(cv_image, dim)
-                        timestr = str(msg.header.stamp.to_sec())
-                        image_info = timestr + " right/" + timestr + ".png\n"
-                        right_file.write(image_info)
-                        image_name = path + "right/" + timestr + ".png"
-                        cv2.imwrite(image_name, cv_image)
-                        pbar.update(1)
+                        elif topic == "/camera_right/image_raw":
+                            try:
+                                cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+                                # cv_image = self.bridge.imgmsg_to_cv2(msg, "mono8")
+                            except CvBridgeError as e:
+                                print(e)
+                            # cv_image_resized = cv2.resize(cv_image, dim)
+                            timestr = str(msg.header.stamp.to_sec())
+                            image_info = timestr + " right/" + timestr + ".png\n"
+                            right_file.write(image_info)
+                            image_name = path + "right/" + timestr + ".png"
+                            cv2.imwrite(image_name, cv_image)
+                            pbar.update(1)
 
-                    elif topic == "/imu/data":
-                        timestr = str(msg.header.stamp.to_sec())
-                        imu_info = timestr + " " + str(msg.linear_acceleration.x) + " " + str(
-                            msg.linear_acceleration.y) + " " + str(msg.linear_acceleration.z)
-                        imu_info = imu_info + " " + str(msg.angular_velocity.x) + " " + str(
-                            msg.angular_velocity.y) + " " + str(msg.angular_velocity.z) + "\n"
-                        imu_file.write(imu_info)
-                        pbar.update(1)
+                        elif topic == "/imu/data":
+                            timestr = str(msg.header.stamp.to_sec())
+                            imu_info = timestr + " " + str(msg.linear_acceleration.x) + " " + str(
+                                msg.linear_acceleration.y) + " " + str(msg.linear_acceleration.z)
+                            imu_info = imu_info + " " + str(msg.angular_velocity.x) + " " + str(
+                                msg.angular_velocity.y) + " " + str(msg.angular_velocity.z) + "\n"
+                            imu_file.write(imu_info)
+                            pbar.update(1)
 
-                    # elif topic == "/raw_odom":
-                    #     timestr = "%.6f" % msg.header.stamp.to_sec()
-                    #     encoder_info = timestr + " " + str(msg.twist.twist.linear.x) + " " + str(
-                    #         msg.twist.twist.linear.y) + " " + str(msg.twist.twist.linear.y) \
-                    #         + " " + str(msg.twist.twist.angular.x) + " " + str(
-                    #         msg.twist.twist.angular.y) + " " + str(msg.twist.twist.angular.y) + "\n"
-                    #     encoder_file.write(encoder_info)
+                        # elif topic == "/raw_odom":
+                        #     timestr = "%.6f" % msg.header.stamp.to_sec()
+                        #     encoder_info = timestr + " " + str(msg.twist.twist.linear.x) + " " + str(
+                        #         msg.twist.twist.linear.y) + " " + str(msg.twist.twist.linear.y) \
+                        #         + " " + str(msg.twist.twist.angular.x) + " " + str(
+                        #         msg.twist.twist.angular.y) + " " + str(msg.twist.twist.angular.y) + "\n"
+                        #     encoder_file.write(encoder_info)
 
         left_file.close()
         right_file.close()
